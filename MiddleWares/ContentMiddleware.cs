b4ac1e65f0a939a82c1b3de435906f5e90e8179f
CarlaY <47314411+CarlaY-Dev@users.noreply.github.com>
2023-11-22 00:59:04 +0330

    test 4 types of middleware in ASP.net Core

---
+++ MiddleWares/ContentMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace MiddeleWares.MiddleWares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ContentMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ContentMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+            if (httpContext.Request.Path.ToString().ToLower().Contains("/content"))
+            {
+                await httpContext.Response.WriteAsync("This message from content...!");
+            }
+           
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ContentMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseContentMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ContentMiddleware>();
+        }
+    }
+}
---
+++ MiddleWares/ShortCircuiMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiddeleWares.MiddleWares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ShortCircuiMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ShortCircuiMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+           
+            await _next(httpContext);
+            if (httpContext.Request.Headers["User-Agent"].Any(p => p.ToLower().Contains("chrome")))
+            {
+                httpContext.Response.StatusCode = 403;
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ShortCircuiMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseShortCircuiMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ShortCircuiMiddleware>();
+        }
+    }
+}
